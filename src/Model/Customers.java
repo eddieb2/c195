84f31e96da04fd79bde91049cde970c0dbbe0983
Edward Blanciak <blanciak.edward2@gmail.com>
2022-11-11 15:30:56 -0500

    customers class setup

--- src/Model/Customers.java
+++ src/Model/Customers.java
@@ -1,4 +1,106 @@
 package Model;
 
 public class Customers {
+
+    private Integer customerId;
+    private String customerName;
+    private String address;
+    private String postalCode;
+    private String phone;
+    private Integer divisionId;
+
+    public Customers(Integer customerId, String customerName, String address, String postalCode, String phone, Integer divisionId) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.address = address;
+        this.postalCode = postalCode;
+        this.phone = phone;
+        this.divisionId = divisionId;
+    }
+
+    /**
+     * @return customerId
+     */
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    /**
+     * @param customerId
+     */
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    /**
+     * @return customerName
+     */
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    /**
+     * @param customerName
+     */
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    /**
+     * @return address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return postalCode
+     */
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    /**
+     * @param postalCode
+     */
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    /**
+     * @return phone
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * @param phone
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * @return divisionId
+     */
+    public Integer getDivisionId() {
+        return divisionId;
+    }
+
+    /**
+     * @param divisionId
+     */
+    public void setDivisionId(Integer divisionId) {
+        this.divisionId = divisionId;
+    }
+
+
 }
