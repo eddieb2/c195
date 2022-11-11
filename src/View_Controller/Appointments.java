1fb405f40c1d02cb38514c81d7e61b578fce103b
Edward Blanciak <blanciak.edward2@gmail.com>
2022-11-11 16:33:28 -0500

    displays all appointments

--- src/View_Controller/Appointments.java
+++ src/View_Controller/Appointments.java
@@ -1,4 +1,60 @@
 package View_Controller;
 
-public class Appointments {
+import DAO.AppointmentQueries;
+import DAO.CustomerQueries;
+import Model.Appointment;
+import Model.Customer;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+public class Appointments implements Initializable {
+
+
+    @FXML private TableView<Appointment> appointmentsTable;
+    @FXML private TableColumn<Appointment,String> idCol;
+    @FXML private TableColumn<Appointment,Integer> titleCol;
+    @FXML private TableColumn<Appointment,String> descriptionCol;
+    @FXML private TableColumn<Appointment,String> locationCol;
+    @FXML private TableColumn<Appointment,String> typeCol;
+    @FXML private TableColumn<Appointment,Date> startCol;
+    @FXML private TableColumn<Appointment,Date> endCol;
+    @FXML private TableColumn<Appointment,Integer> customerIdCol;
+    @FXML private TableColumn<Appointment,Integer> userIdCol;
+    @FXML private TableColumn<Appointment,Integer> contactIdCol;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        idCol.setCellValueFactory(new PropertyValueFactory<>("appointmentId"));
+        titleCol.setCellValueFactory(new PropertyValueFactory<>("title"));
+        descriptionCol.setCellValueFactory(new PropertyValueFactory<>("description"));
+        locationCol.setCellValueFactory(new PropertyValueFactory<>("location"));
+        typeCol.setCellValueFactory(new PropertyValueFactory<>("type"));
+        startCol.setCellValueFactory(new PropertyValueFactory<>("start"));
+        endCol.setCellValueFactory(new PropertyValueFactory<>("end"));
+        customerIdCol.setCellValueFactory(new PropertyValueFactory<>("customerId"));
+        userIdCol.setCellValueFactory(new PropertyValueFactory<>("userId"));
+        contactIdCol.setCellValueFactory(new PropertyValueFactory<>("contactId"));
+
+        // Queries the database and saves all the appointments to an observable list.
+        ObservableList<Appointment> appointments = null;
+
+        try {
+            appointments = AppointmentQueries.getAllAppointments();
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
+
+        appointmentsTable.setItems(appointments);
+    }
 }
