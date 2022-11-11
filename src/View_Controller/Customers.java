d92b43669581011ab66c1b25764b2b458b275195
Edward Blanciak <blanciak.edward2@gmail.com>
2022-11-11 15:32:35 -0500

    customers controller and view setup

--- src/View_Controller/Customers.java
+++ src/View_Controller/Customers.java
@@ -1,2 +1,12 @@
-package View_Controller;public class Customers {
+package View_Controller;
+
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
+public class Customers {
+
+    public AnchorPane customersTab;
+    public TableView customersTable;
+    public TableColumn idCol;
 }
