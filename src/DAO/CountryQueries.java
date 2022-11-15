7d1b9c4ca1c2fcdca7d85732f481e69d3ce33a2f
Edward Blanciak <blanciak.edward2@gmail.com>
2022-11-14 21:36:57 -0500

    Adds a query to read all countries from the db.

--- src/DAO/CountryQueries.java
+++ src/DAO/CountryQueries.java
@@ -1,2 +1,29 @@
-package DAO;public class CountryQueries {
+package DAO;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import src.model.Country;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CountryQueries {
+
+    public static ObservableList<Country> getAllCountries() throws SQLException {
+        ObservableList<Country> countries = FXCollections.observableArrayList();
+
+        String sql = "SELECT * FROM countries";
+        PreparedStatement ps = DBConnection.connection.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+
+        while(rs.next()) {
+            Integer countryId = rs.getInt("country_id");
+            String country = rs.getString("country");
+
+            countries.add(new Country(countryId, country));
+        }
+
+        return countries;
+    }
 }
