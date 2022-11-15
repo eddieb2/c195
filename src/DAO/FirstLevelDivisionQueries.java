7b5099638a8442a7ba6a3f4092c437b67732efe8
Edward Blanciak <blanciak.edward2@gmail.com>
2022-11-14 21:39:58 -0500

    Adds a query to retrieve all fld.

--- src/DAO/FirstLevelDivisionQueries.java
+++ src/DAO/FirstLevelDivisionQueries.java
@@ -1,2 +1,30 @@
-package DAO;public class FirstLevelDivisionQueries {
+package DAO;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import src.model.FirstLevelDivision;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class FirstLevelDivisionQueries {
+
+    public static ObservableList<FirstLevelDivision> getAllDivisions() throws SQLException {
+        ObservableList<FirstLevelDivision> divisions = FXCollections.observableArrayList();
+
+        String sql = "SELECT * FROM first_level_divisions";
+        PreparedStatement ps = DBConnection.connection.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+
+        while(rs.next()) {
+            Integer divisionId = rs.getInt("division_id");
+            String division = rs.getString("division");
+            Integer countryId = rs.getInt("country_id");
+
+            divisions.add(new FirstLevelDivision(divisionId, division, countryId));
+        }
+
+        return divisions;
+    }
 }
