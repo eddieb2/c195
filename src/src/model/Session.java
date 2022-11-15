3630e79fc1c2975aebbcce80792297a1c428eac0
Edward Blanciak <blanciak.edward2@gmail.com>
2022-11-14 21:46:57 -0500

    Adds a session class for logging on. It is currently not in use.

--- src/src/model/Session.java
+++ src/src/model/Session.java
@@ -1,2 +1,21 @@
-package src.model;public class Application {
+package src.model;
+
+public class Session {
+    private User autheticatedUser;
+
+    public Session() {
+
+    }
+
+    public Session(User autheticatedUser) {
+        this.autheticatedUser = autheticatedUser;
+    }
+
+    public User getAutheticatedUser() {
+        return autheticatedUser;
+    }
+
+    public void setAutheticatedUser(User autheticatedUser) {
+        this.autheticatedUser = autheticatedUser;
+    }
 }
