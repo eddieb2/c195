684e1e5c70bfbd9ded484c942ef1ffc70baf4813
Edward Blanciak <blanciak.edward2@gmail.com>
2022-11-14 21:36:22 -0500

    Classes created for country and fld.

--- src/src/model/Country.java
+++ src/src/model/Country.java
@@ -1,2 +1,27 @@
-package src.model;public class Country {
+package src.model;
+
+public class Country {
+    private Integer countryId;
+    private String country;
+
+    public Country(Integer countryId, String country) {
+        this.countryId = countryId;
+        this.country = country;
+    }
+
+    public Integer getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(Integer countryId) {
+        this.countryId = countryId;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
--- src/src/model/FirstLevelDivision.java
+++ src/src/model/FirstLevelDivision.java
@@ -1,2 +1,37 @@
-package src.model;public class FirstLevelDivision {
+package src.model;
+
+public class FirstLevelDivision {
+    private Integer divisionId;
+    private String division;
+    private Integer countryId;
+
+    public FirstLevelDivision(Integer divisionId, String division, Integer countryId) {
+        this.divisionId = divisionId;
+        this.division = division;
+        this.countryId = countryId;
+    }
+
+    public Integer getDivisionId() {
+        return divisionId;
+    }
+
+    public void setDivisionId(Integer divisionId) {
+        this.divisionId = divisionId;
+    }
+
+    public String getDivision() {
+        return division;
+    }
+
+    public void setDivision(String division) {
+        this.division = division;
+    }
+
+    public Integer getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(Integer countryId) {
+        this.countryId = countryId;
+    }
 }
