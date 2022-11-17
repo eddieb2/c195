574c4b8c8b8229dc8880b660de25b052da264e85
Edward Blanciak <blanciak.edward2@gmail.com>
2022-11-16 21:36:47 -0500

    Adds skeleton for update appointment form.

---
+++ src/view_controller/dashboard/appointments/UpdateAppointmentForm.java
@@ -0,0 +1,15 @@
+package view_controller.dashboard.appointments;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class UpdateAppointmentForm {
+    public Button submitButton;
+    public Button cancelButton;
+
+    public void submitForm(ActionEvent event) {
+    }
+
+    public void closeForm(ActionEvent event) {
+    }
+}
