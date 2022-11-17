118fdcfbda578b9ede33a01c2865b7f3582e5637
Edward Blanciak <blanciak.edward2@gmail.com>
2022-11-16 21:41:45 -0500

    Adds more columns to the tableview.

--- src/view_controller/dashboard/appointments/AppointmentsTabController.java
+++ src/view_controller/dashboard/appointments/AppointmentsTabController.java
@@ -1,37 +1,42 @@
 package view_controller.dashboard.appointments;
 
 import DAO.AppointmentQueries;
-import src.model.Appointment;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import src.model.Appointment;
 
 import java.net.URL;
 import java.sql.SQLException;
-import java.util.Date;
+import java.sql.Timestamp;
 import java.util.ResourceBundle;
 
-public class AppointmentsController implements Initializable {
-
-
+public class AppointmentsTabController implements Initializable {
     @FXML private TableView<Appointment> appointmentsTable;
     @FXML private TableColumn<Appointment,String> idCol;
     @FXML private TableColumn<Appointment,Integer> titleCol;
     @FXML private TableColumn<Appointment,String> descriptionCol;
     @FXML private TableColumn<Appointment,String> locationCol;
     @FXML private TableColumn<Appointment,String> typeCol;
-    @FXML private TableColumn<Appointment,Date> startCol;
-    @FXML private TableColumn<Appointment,Date> endCol;
+    @FXML private TableColumn<Appointment,Timestamp> startCol;
+    @FXML private TableColumn<Appointment,Timestamp> endCol;
+    @FXML private TableColumn<Appointment, Timestamp> createDateCol;
+    @FXML private TableColumn<Appointment, String> createdByCol;
+    @FXML private TableColumn<Appointment, Timestamp> lastUpdateCol;
+    @FXML private TableColumn<Appointment, String> lastUpdatedByCol;
     @FXML private TableColumn<Appointment,Integer> customerIdCol;
     @FXML private TableColumn<Appointment,Integer> userIdCol;
     @FXML private TableColumn<Appointment,Integer> contactIdCol;
 
+    public static Appointment selectedAppointment;
+    public static ObservableList<Appointment> appointments;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Table column properties
         idCol.setCellValueFactory(new PropertyValueFactory<>("appointmentId"));
         titleCol.setCellValueFactory(new PropertyValueFactory<>("title"));
         descriptionCol.setCellValueFactory(new PropertyValueFactory<>("description"));
@@ -39,20 +44,26 @@ public class AppointmentsController implements Initializable {
         typeCol.setCellValueFactory(new PropertyValueFactory<>("type"));
         startCol.setCellValueFactory(new PropertyValueFactory<>("start"));
         endCol.setCellValueFactory(new PropertyValueFactory<>("end"));
+        createDateCol.setCellValueFactory(new PropertyValueFactory<>("createDate"));
+        createdByCol.setCellValueFactory(new PropertyValueFactory<>("createdBy"));
+        lastUpdateCol.setCellValueFactory(new PropertyValueFactory<>("lastUpdate"));
+        lastUpdatedByCol.setCellValueFactory(new PropertyValueFactory<>("lastUpdatedBy"));
         customerIdCol.setCellValueFactory(new PropertyValueFactory<>("customerId"));
         userIdCol.setCellValueFactory(new PropertyValueFactory<>("userId"));
         contactIdCol.setCellValueFactory(new PropertyValueFactory<>("contactId"));
 
-        // Queries the database and saves all the appointments to an observable list.
-        ObservableList<Appointment> appointments = null;
+        // Listens for a appointment selection change
+        appointmentsTable.getSelectionModel().selectedItemProperty().addListener((observableValue, part, t1) -> {
+            selectedAppointment = appointmentsTable.getSelectionModel().getSelectedItem();
+        });
 
+        // Queries the database and saves all the appointments/users/contacts to an observable list.
+        // Populates the appointments table view with appointments-
         try {
             appointments = AppointmentQueries.getAllAppointments();
-        } catch (SQLException throwable) {
-            throwable.printStackTrace();
-            System.out.println(throwable);
+            appointmentsTable.setItems(appointments);
+        } catch (SQLException e){
+            e.printStackTrace();
         }
-
-        appointmentsTable.setItems(appointments);
     }
 }
